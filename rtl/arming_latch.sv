95eb66b5435ffe0182de49713f3b33f8a48c7b8e
hashtagjeff <harsh.hash2018.singh@gmail.com>
2026-04-27 12:14:57 +0000

    switched into the offical release

---
+++ rtl/arming_latch.sv
@@ -0,0 +1,10 @@
+module arming_latch(input clk, arm, disarm, output reg armed);
+    initial armed = 1'b0;
+    always @(posedge clk) begin
+        if(disarm) armed <= 1'b0;
+        else if (arm) armed <= 1'b1;
+    end 
+endmodule 
+
+
+
