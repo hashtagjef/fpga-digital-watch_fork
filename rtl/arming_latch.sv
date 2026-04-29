146dceb41946dc425bfa67d6e343d4437c8f3ecb
hashtagjeff <harsh.hash2018.singh@gmail.com>
2026-04-29 23:26:38 +0000

    bug fixes

--- rtl/arming_latch.sv
+++ rtl/arming_latch.sv
@@ -1,10 +1,7 @@
 module arming_latch(input clk, arm, disarm, output reg armed);
     initial armed = 1'b0;
-    always @(posedge clk) begin
+    always_ff @(posedge clk) begin
         if(disarm) armed <= 1'b0;
         else if (arm) armed <= 1'b1;
-    end 
-endmodule 
-
-
-
+    end
+endmodule
