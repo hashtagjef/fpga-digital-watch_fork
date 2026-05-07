b874800648c1a669a01216254c59a15fa86f5321
hashtagjeff <harsh.hash2018.singh@gmail.com>
2026-05-07 05:17:51 +0000

    added arming latch

--- rtl/arming_latch.sv
+++ rtl/arming_latch.sv
@@ -8,11 +8,16 @@
 //  arm    - Sets armed high on the next rising clock edge.
 //  disarm - Clears armed low on the next rising clock edge.
 //  armed  - Latched armed state.
-
-module arming_latch(input clk, arm, disarm, output reg armed);
-    initial armed = 1'b0;
-    always_ff @(posedge clk) begin
-        if(disarm) armed <= 1'b0;
-        else if (arm) armed <= 1'b1;
-    end
+`timescale 1ns / 1ps
+module arming_latch (
+    input  logic clk,
+    input  logic arm,
+    input  logic disarm,
+    output logic armed
+);
+  initial armed = 1'b0;
+  always_ff @(posedge clk) begin
+    if (disarm) armed <= 1'b0;
+    else if (arm) armed <= 1'b1;
+  end
 endmodule
